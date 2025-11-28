1bdace08b99844f521144e30e2fbb713e7f37a6d
随风而散 <13459528+suifengersan@user.noreply.gitee.com>
2025-11-28 10:01:17 +0800

    init

---
+++ src/main/java/com/sfes/controller/CommonController.java
@@ -0,0 +1,18 @@
+package com.sfes.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+    @PostMapping("/file")
+    public String upload(MultipartFile file){
+        System.out.println(file);
+        return "123";
+    }
+}
