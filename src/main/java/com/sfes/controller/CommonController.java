0b8ab0575cc31fcfc7fc8ce86b1809f6848cbfe7
随风而散 <13459528+suifengersan@user.noreply.gitee.com>
2025-11-28 10:08:18 +0800

    helloworld2

--- src/main/java/com/sfes/controller/CommonController.java
+++ src/main/java/com/sfes/controller/CommonController.java
@@ -1,6 +1,7 @@
 package com.sfes.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class CommonController {
         System.out.println(file);
         int a = 1;//测试
         return "123";
+    }
+
+    @GetMapping()
+    public String test(){
+        return "hello world";
     }
 }
